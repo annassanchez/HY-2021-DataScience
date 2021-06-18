1e9fd859145b42408f5ca0fa224dee4371b9e862
annassanchez <51114581+annassanchez@users.noreply.github.com>
2021-06-18 23:08:10 +0200

    new file added

---
+++ week01/part01-e14_find_matching/src/find_matching.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def find_matching(L, pattern):
+    lista = []
+    for i, x in enumerate(L):
+        if pattern in x:
+            lista.append(i)
+        else:
+            lista
+    return lista
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
