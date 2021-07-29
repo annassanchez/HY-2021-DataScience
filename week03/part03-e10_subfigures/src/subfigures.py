33ae0c81901326014936688438a6cfd59b40431f
annassanchez <51114581+annassanchez@users.noreply.github.com>
2021-07-29 18:00:03 +0200

    added root file for part03-e10

---
+++ week03/part03-e10_subfigures/src/subfigures.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def subfigures(a):
+    plt.subplot(1, 2, 1)
+    plt.plot(a[:,0],a[:,1])
+    plt.subplot(1, 2, 2)
+    plt.scatter(a[:,0], a[:,1], c = a[:,2], s = a[:,3])
+    return plt.show()
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
