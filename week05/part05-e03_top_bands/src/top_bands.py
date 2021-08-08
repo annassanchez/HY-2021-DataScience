9a56de992fbe75fb17776a57d8ef3230c3e2ad01
annassanchez <51114581+annassanchez@users.noreply.github.com>
2021-08-08 13:01:19 +0200

    root file aded for part05-e03

--- week05/part05-e03_top_bands/src/top_bands.py
+++ week05/part05-e03_top_bands/src/top_bands.py
@@ -3,10 +3,18 @@
 import pandas as pd
 
 def top_bands():
-    return pd.DataFrame()
+  #url_top40 = 'https://raw.githubusercontent.com/annassanchez/HY-2021-DataScience/main/week05/UK-top40-1964-1-2.tsv'
+  #url_bands = 'https://raw.githubusercontent.com/annassanchez/HY-2021-DataScience/main/week05/bands.tsv'
+  df_top40 = pd.read_csv('src/UK-top40-1964-1-2.tsv', sep='\t')
+  df_top40['Artist'] = df_top40['Artist'].str.title()
+  df_bands = pd.read_csv('src/bands.tsv', sep='\t')
+  df_topBands = pd.merge(df_top40, df_bands, left_on='Artist', right_on='Band', how = 'right')
+  return df_topBands
 
 def main():
-    return
+    df = top_bands()
+    print("Shape:", df.shape)
+    print("Columns:", df.columns)
 
 if __name__ == "__main__":
     main()
