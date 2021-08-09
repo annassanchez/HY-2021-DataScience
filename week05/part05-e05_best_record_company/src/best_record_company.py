9486f8856f8e714f4af57699415909a4fabf80a7
annassanchez <51114581+annassanchez@users.noreply.github.com>
2021-08-09 18:38:37 +0200

    root file for aprt05-e05

--- week05/part05-e05_best_record_company/src/best_record_company.py
+++ week05/part05-e05_best_record_company/src/best_record_company.py
@@ -3,10 +3,18 @@
 import pandas as pd
 
 def best_record_company():
-    return None
+    company = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep="\t")
+    comps = company.groupby("Publisher")
+    wkcomps = comps["WoC"].sum()
+    wkcomps = wkcomps.sort_values(ascending=False)
+    publ = wkcomps.head(1)
+    p=publ.index[0]
+    topsingles = company[company["Publisher"] == p]
+    #print(wkcomps.index[wkcomps[0]].tolist())
+    return topsingles
 
 def main():
-    return
+    best_record_company()
     
 
 if __name__ == "__main__":
