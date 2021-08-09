c4000a72864b816de413355b3364bbd518445a60
annassanchez <51114581+annassanchez@users.noreply.github.com>
2021-08-09 19:04:48 +0200

    root file for part05-e06 added

--- week05/part05-e06_suicide_fractions/src/suicide_fractions.py
+++ week05/part05-e06_suicide_fractions/src/suicide_fractions.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
 import pandas as pd
+import numpy as np
 
 def suicide_fractions():
-    return pd.Series()
+    df_suicide = pd.read_csv('src/who_suicide_statistics.csv', sep = ',')
+    df_suicide['suicide_population'] = df_suicide['suicides_no'] / df_suicide['population']
+    suicide = df_suicide.groupby(by = 'country')['suicide_population'].mean()
+    return suicide
 
 def main():
-    return
+    suicide_fractions()
 
 if __name__ == "__main__":
     main()
