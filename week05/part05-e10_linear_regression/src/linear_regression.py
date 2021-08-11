be4affe8755deed3b87cb1ff3dfd3c9130892bb6
annassanchez <51114581+annassanchez@users.noreply.github.com>
2021-08-11 19:03:37 +0200

    root file added for part05-e10

--- week05/part05-e10_linear_regression/src/linear_regression.py
+++ week05/part05-e10_linear_regression/src/linear_regression.py
@@ -5,9 +5,28 @@ from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
 def fit_line(x, y):
-    return 0, 0
+    model_linear=LinearRegression(fit_intercept=True)
+    model_linear.fit(np.vstack([x]).T, y)
+    #xf = x
+    #yf_linear = model_linear.predict(np.vstack([x]).T)
+    slope =  model_linear.coef_[0]
+    intercept = model_linear.intercept_
+    return slope, intercept
     
 def main():
+    x = np.linspace(-50,150,50)
+    y = x    
+    a, b = fit_line(x, y)
+    print("Slope:", a)
+    print("Intercept:", b)
+    plt.scatter(x, y, color="black")
+    model_linear=LinearRegression(fit_intercept=True)
+    model_linear.fit(np.vstack([x]).T, y)
+    xf = x
+    yf = model_linear.predict(np.vstack([x]).T)
+    plt.plot(xf,yf, label="linear")
+    plt.show()
+
     
 if __name__ == "__main__":
     main()
