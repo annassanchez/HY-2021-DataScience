3774f5c3b9eee7b3d0be8b5e0f94a2fc84a5e599
annassanchez <51114581+annassanchez@users.noreply.github.com>
2021-08-31 11:30:46 +0200

    root file added for paret05-e12

--- week05/part05-e12_coefficient_of_determination/src/coefficient_of_determination.py
+++ week05/part05-e12_coefficient_of_determination/src/coefficient_of_determination.py
@@ -2,12 +2,42 @@
 
 import pandas as pd
 from sklearn import linear_model
+import numpy as np
 
 
 def coefficient_of_determination():
-    return []
+    df = pd.read_csv("src/mystery_data.tsv", sep="\t")
+    X = df.loc[:,"X1":"X5"]
+    y = df.Y
+ 
+    x1 = np.array(df.loc[:,"X1"]).reshape(-1,1)
+    x2 = np.array(df.loc[:,"X2"]).reshape(-1,1)
+    x3 = np.array(df.loc[:,"X3"]).reshape(-1,1)
+    x4 = np.array(df.loc[:,"X4"]).reshape(-1,1)
+    x5 = np.array(df.loc[:,"X5"]).reshape(-1,1)
+
+    reg_all = linear_model.LinearRegression().fit(X, y)
+    reg_x1 = linear_model.LinearRegression().fit(x1, y)
+    reg_x2 = linear_model.LinearRegression().fit(x2, y)
+    reg_x3 = linear_model.LinearRegression().fit(x3, y)
+    reg_x4 = linear_model.LinearRegression().fit(x4, y)
+    reg_x5 = linear_model.LinearRegression().fit(x5, y)
+
+    r_all = reg_all.score(X,y)
+    r_x1 = reg_x1.score(x1, y)
+    r_x2 = reg_x2.score(x2, y)
+    r_x3 = reg_x3.score(x3, y)
+    r_x4 = reg_x4.score(x4, y)
+    r_x5 = reg_x5.score(x5, y)
+    return [r_all, r_x1, r_x2, r_x3, r_x4, r_x5]
+
+ 
     
 def main():
+    results = coefficient_of_determination()
+    #print(f'R2-score with feature(s) X: {results[0]}')
+    for index, item in enumerate(results[1:]):
+        print(f'R2-score with feature(s) X{index}: {item}')
 
 if __name__ == "__main__":
     main()
