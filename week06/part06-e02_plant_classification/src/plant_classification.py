107124bf91d45dccea7bfd09dc0b2c367a51d4da
annassanchez <51114581+annassanchez@users.noreply.github.com>
2021-09-01 19:08:41 +0200

    root file added for part06-e02

--- week06/part06-e02_plant_classification/src/plant_classification.py
+++ week06/part06-e02_plant_classification/src/plant_classification.py
@@ -6,7 +6,18 @@ from sklearn import naive_bayes
 from sklearn import metrics
 
 def plant_classification():
-    return 0.0
+    #1_load dataset
+    X, y = load_iris(return_X_y = True)
+    #2_split into testing and training
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state=0)
+    #3_fit training daata with Gaussian NB
+    model = naive_bayes.GaussianNB()
+    model.fit(X_train, y_train)
+    #4_pedict labels
+    labels_fitted = model.predict(X_test)
+    #5_accuracy
+    accuracy = metrics.accuracy_score(y_test, labels_fitted)
+    return accuracy
 
 def main():
     print(f"Accuracy is {plant_classification()}")
