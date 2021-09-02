c3b967bb3d7151d13bfeecbf5935645d6ccf1866
annassanchez <51114581+annassanchez@users.noreply.github.com>
2021-09-02 20:14:24 +0200

    root file added for part06-e03

--- week06/part06-e03_word_classification/src/word_classification.py
+++ week06/part06-e03_word_classification/src/word_classification.py
@@ -7,6 +7,7 @@ from lxml import etree
 import numpy as np
 
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import cross_val_score
 from sklearn import model_selection
 
@@ -37,18 +38,40 @@ def load_english():
     return lines
 
 def get_features(a):
-    return np.array([[]])
+    columns=len(alphabet)
+    n=a.shape[0]
+    f=np.zeros((n, columns))
+    for i, s in enumerate(a):
+        counts=Counter(s)
+        for j, c in enumerate(alphabet):
+            f[i, j] = counts[c]
+    return f
 
 def contains_valid_chars(s):
-    return True
+    return alphabet_set.issuperset(s)
 
 def get_features_and_labels():
-    return np.array([[]]), np.array([])
-
+    lst=load_finnish()
+    finnish=np.array(list(filter(contains_valid_chars, map(lambda s: s.lower(), lst))))
+    lines = load_english()
+    lst=filter(contains_valid_chars,
+               map(lambda s: s.lower(),
+                   filter(lambda s: s[0].islower(), lines)))
+    english=np.array(list(lst))
+    n1=finnish.shape[0]
+    f1=get_features(finnish)
+    n2=english.shape[0]
+    f2=get_features(english)
+    y=np.hstack([[0]*n1, [1]*n2])
+    X = np.vstack([f1, f2])
+    return X, y
 
 def word_classification():
-    return []
-
+    X, y = get_features_and_labels()
+    model = MultinomialNB()
+    cv = model_selection.KFold(n_splits=5, shuffle=True, random_state=0)
+    v=cross_val_score(model, X, y, cv=cv)
+    return v
 
 def main():
     print("Accuracy scores are:", word_classification())
