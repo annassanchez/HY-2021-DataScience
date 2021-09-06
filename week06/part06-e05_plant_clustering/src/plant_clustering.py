db17309241da652915d7d32cab35c32971199ace
annassanchez <51114581+annassanchez@users.noreply.github.com>
2021-09-06 17:59:37 +0200

    added root file for part06-e05

--- week06/part06-e05_plant_clustering/src/plant_clustering.py
+++ week06/part06-e05_plant_clustering/src/plant_clustering.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
+from sklearn.datasets import load_iris
+
+from sklearn import metrics
+
 import scipy
+import numpy as np
+from sklearn.cluster import KMeans
 
 
 def find_permutation(n_clusters, real_labels, labels):
@@ -13,7 +19,18 @@ def find_permutation(n_clusters, real_labels, labels):
     return permutation
 
 def plant_clustering():
-    return 0.0
+    #1_load dataset
+    X, y = load_iris(return_X_y = True)
+    #3_fit training daata with Gaussian NB
+    model = KMeans(3, random_state=0)
+    model.fit(X)
+    #4_fix y with permutation
+    permutation = find_permutation(3, y, model.labels_)
+    #5_obtain new labels
+    new_labels = [ permutation[label] for label in model.labels_]   # permute the labels
+    #6_calculate accuracy
+    accuracy = metrics.accuracy_score(y, new_labels)
+    return accuracy
 
 def main():
     print(plant_clustering())
