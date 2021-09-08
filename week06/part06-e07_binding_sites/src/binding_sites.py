3e0ec28504ed7f1d95b8a72dd871fd491cd8183a
annassanchez <51114581+annassanchez@users.noreply.github.com>
2021-09-08 20:08:42 +0200

    root file added for part06-e07

--- week06/part06-e07_binding_sites/src/binding_sites.py
+++ week06/part06-e07_binding_sites/src/binding_sites.py
@@ -12,12 +12,22 @@ import seaborn as sns
 sns.set(color_codes=True)
 import scipy.spatial as sp
 import scipy.cluster.hierarchy as hc
+from scipy.stats import mode
 
 def toint(x):
-    return 0
+    base = ["A", "C", "G", "T"]
+    idx = [0, 1, 2, 3]
+    d = dict(zip(base, idx))
+    return d[x]
 
 def get_features_and_labels(filename):
-    return (np.array([[]]), np.array([]))
+    df = pd.read_csv(filename, sep='\t')
+    # apply the 'toint' function to each letter 'l' in string 'x' contained in column 'X'
+    df["X"] = df["X"].apply(lambda x: [toint(l) for l in list(x)])
+    X = np.array(df["X"])
+    X = np.asarray([np.asarray(row, dtype=int) for row in X], dtype=int)
+    y = np.array(df["y"])
+    return (X,y) # (np.array(df["X"]), np.array(df["y"]))
 
 def plot(distances, method='average', affinity='euclidean'):
     mylinkage = hc.linkage(sp.distance.squareform(distances), method=method)
@@ -26,11 +36,41 @@ def plot(distances, method='average', affinity='euclidean'):
     plt.show()
 
 def cluster_euclidean(filename):
-    return 0.0
+    X, y = get_features_and_labels(filename)
+    X = np.asarray([np.asarray(row, dtype=int) for row in X], dtype=int)
+    y = np.asarray([np.asarray(row, dtype=int) for row in y], dtype=int)
+    model = AgglomerativeClustering(linkage="average", affinity="euclidean")
+    model.fit(X)
+    labels = model.labels_
+    n_clusters_ = len(set(labels))
+    p = find_permutation(n_clusters_, y, labels)
+    new_labels = [p[label] for label in labels]
+    acc_ = accuracy_score(y, new_labels)
+    return acc_
+
+def find_permutation(n_clusters, real_labels, labels):
+    permutation = []
+    for i in range(n_clusters):
+        idx = labels == i
+         # Choose the most common label among data points in the cluster
+        new_label = mode(real_labels[idx])[0][0]
+        permutation.append(new_label)
+    return permutation
 
 def cluster_hamming(filename):
-    return 0.0
-
+    X, y = get_features_and_labels(filename)
+    X = np.asarray([np.asarray(row, dtype=int) for row in X], dtype=int)
+    y = np.asarray([np.asarray(row, dtype=int) for row in y], dtype=int)
+    hamming_dist = pairwise_distances(X, metric="hamming")
+    # If “precomputed”, a distance matrix (instead of a similarity matrix) is needed as input for the fit method.
+    model = AgglomerativeClustering(linkage="average", affinity="precomputed")
+    model.fit(hamming_dist)
+    labels = model.labels_
+    n_clusters_ = len(set(labels))
+    p = find_permutation(n_clusters_, y, labels)
+    new_labels = [p[label] for label in labels]
+    acc_ = accuracy_score(y, new_labels)
+    return acc_
 
 def main():
     print("Accuracy score with Euclidean affinity is", cluster_euclidean("src/data.seq"))
